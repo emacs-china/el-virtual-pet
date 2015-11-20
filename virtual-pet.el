64c8d3c2e491755ca81b141eedbc91f5e0153605
darksun <lujun9972@sina.com>
2015-11-20 23:04:23 +0800

    make GUI

---
+++ virtual-pet.el
@@ -0,0 +1,80 @@
+(defun vp--insert-image-button (label &optional img &rest properties)
+  (let ((btn (apply #'insert-text-button label properties)))
+    (when img
+      (add-text-properties (button-start btn) (button-end btn)
+                           `(display ,(create-image img))))))
+
+(defun vp--insert-image-function-button (label img fn)
+  (vp--insert-image-button label img 'follow-link t 'action fn))
+
+(defun vp--insert-image (label &optional img width height)
+  (let ((pos-start (point))
+        pos-end)
+    (insert label)
+    (setq pos-end (point))
+    (when img
+      (if (or width height)
+          (add-text-properties pos-start pos-end `(display ,(create-image img 'imagemagick nil :width width :height height)))
+        (add-text-properties pos-start pos-end `(display ,(create-image img)))))))
+
+(defun vp--insert-process-bar (label current max)
+  (let ((process-bar (format "%-10s:%s%s" label (make-string current ?*) (make-string (- max current) ?\ ))))
+    (insert process-bar)))
+
+
+(defcustom vp-buffer "*virtual-pet*"
+  "name of virtual pet buffer")
+
+(defstruct virtual-pet
+  (name (read-string "please input the pet's name:"))
+  (hunger 0)
+  (happiness 20)
+  (health 20)
+  (status 'stop))
+
+(defvar vp-the-pet (make-virtual-pet))
+
+(defun vp--redraw-action-buttons ()
+  (goto-char (point-min))
+  (ignore-errors 
+    (kill-whole-line))
+  (dolist (action '("stop" "sick" "play" "walk" "eat"))
+    (insert "\t")
+    (let ((btn-img (format "%s%s-button.GIF" default-directory action)))
+      (vp--insert-image-function-button action btn-img (lambda (btn)
+                                                         (setf (virtual-pet-status vp-the-pet)
+                                                               (intern (button-label btn)))
+                                                         (vp--redraw-pet vp-the-pet)))))
+  (newline))
+
+(defun vp--redraw-pet (pet)
+  (goto-char (point-min))
+  (forward-line)
+  (ignore-errors
+    (kill-whole-line))
+  (vp--insert-image (symbol-name (virtual-pet-status pet))
+                    (format "%s%s.jpg" default-directory (virtual-pet-status pet))
+                    450 348)
+  (newline))
+
+
+(defun vp--redraw-pet-status (pet)
+  (goto-char (point-min))
+  (forward-line)
+  (forward-line)
+  (delete-region (point) (point-max))
+  (vp--insert-process-bar "hunger" (virtual-pet-hunger vp-the-pet) 20)
+  (newline)
+  (vp--insert-process-bar "health" (virtual-pet-health vp-the-pet) 20)
+  (newline)
+  (vp--insert-process-bar "happiness" (virtual-pet-happiness vp-the-pet) 20))
+
+
+(defun vp-redraw-gui ()
+  (switch-to-buffer (get-buffer-create vp-buffer))
+  (vp--redraw-action-buttons)
+  (vp--redraw-pet vp-the-pet)
+  (vp--redraw-pet-status vp-the-pet))
+
+
+(vp-redraw-gui)
